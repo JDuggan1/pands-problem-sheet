63bd94a63d52bb3f3e2b45207485a39ed3077ff7
JDuggan1 <G00411376@gmit.ie>
2022-02-09 11:23:41 +0000

    week03

---
+++ week03/week03/Weekly Task 3 (Reassess).py
@@ -0,0 +1,5 @@
+#Write a program that asks a user to input a string and outputs every second letter in reverse order.
+# Author Jonathon Duggan
+sentence = input("Help with the output:")
+print(sentence[::-2])
+
---
+++ week03/week03/x lab3.1.1.py
@@ -0,0 +1,6 @@
+#program that asks a user to input a string and outputs every second letter in reverse order.
+Reference work - https://github.com/g00387822/reverse_string_every_second_letter/commit/8eae5890868e11a1eccc5968b502e555586b3f49
+sentence = input(" I am trying to create a program that outputs a sentences every second letter backwards :")
+print(sentence[::-2])
+
+
